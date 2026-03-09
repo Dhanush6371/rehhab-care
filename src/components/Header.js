3c590daa3ff6eeef5973fc44524c0605207db272
Afterlife24 <afterlife668@gmail.com>
2026-03-10 05:20:04 +0530

    updated5

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         <header className="header">
             <nav className="navbar">
                 <div className="logo">
-                    <img src="/logo.svg" alt="Rebuild Care" />
+                    <img src="/images/logo.png" alt="Rebuild Care" />
                     <span>Rebuild Care</span>
                 </div>
 
